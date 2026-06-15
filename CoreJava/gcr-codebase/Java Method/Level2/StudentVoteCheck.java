175bd9bfc5bcbda492384885e5765b870c074f6e
sachinr69 <sn08064021@gmail.com>
2026-06-15 22:05:42 +0530

    Student Vote Checker

---
+++ CoreJava/gcr-codebase/Java Method/Level2/StudentVoteCheck.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class StudentVoteChecker {
+
+    static boolean canStudentVote(int age){
+
+        if(age < 0)
+            return false;
+
+        return age >= 18;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        int[] ages=new int[10];
+
+        for(int i=0;i<10;i++){
+
+            ages[i]=sc.nextInt();
+
+            if(canStudentVote(ages[i]))
+                System.out.println("Can Vote");
+            else
+                System.out.println("Cannot Vote");
+        }
+    }
+}
