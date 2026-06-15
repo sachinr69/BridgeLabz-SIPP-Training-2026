1b3380f8cb0e33a1b2ea02b75087d120538d4aa0
sachinr69 <sn08064021@gmail.com>
2026-06-15 22:05:12 +0530

    Unit Converter (Yards, Inches)

---
+++ CoreJava/gcr-codebase/Java Method/Level2/UnitConverter.java
@@ -0,0 +1,22 @@
+public class UnitConverter2 {
+
+    static double convertYardsToFeet(double yards){
+        return yards * 3;
+    }
+
+    static double convertFeetToYards(double feet){
+        return feet * 0.333333;
+    }
+
+    static double convertMetersToInches(double meters){
+        return meters * 39.3701;
+    }
+
+    static double convertInchesToMeters(double inches){
+        return inches * 0.0254;
+    }
+
+    static double convertInchesToCm(double inches){
+        return inches * 2.54;
+    }
+}
