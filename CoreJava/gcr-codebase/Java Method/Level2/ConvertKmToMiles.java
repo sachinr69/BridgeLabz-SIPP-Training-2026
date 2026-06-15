390aa374ed058fa51ff96554fedfffda8da60ca5
sachinr69 <sn08064021@gmail.com>
2026-06-15 22:03:49 +0530

    Unit Converter (Km, Miles, Meter, Feet)

---
+++ CoreJava/gcr-codebase/Java Method/Level2/ConvertKmToMiles.java
@@ -0,0 +1,18 @@
+public class UnitConverter {
+
+    static double convertKmToMiles(double km){
+        return km * 0.621371;
+    }
+
+    static double convertMilesToKm(double miles){
+        return miles * 1.60934;
+    }
+
+    static double convertMetersToFeet(double meter){
+        return meter * 3.28084;
+    }
+
+    static double convertFeetToMeters(double feet){
+        return feet * 0.3048;
+    }
+}
