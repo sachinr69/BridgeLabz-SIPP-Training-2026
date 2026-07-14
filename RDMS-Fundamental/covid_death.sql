1a2a0c11d0d0596e3740cbd9016ce693d42c4d25
sachinr69 <sn08064021@gmail.com>
2026-07-14 13:15:34 +0530

    update

---
+++ RDMS-Fundamental/covid_death.sql
@@ -0,0 +1,5 @@
+CREATE TABLE covid_deaths (
+    country VARCHAR(50),
+    report_date DATE,
+    total_deaths INT
+);
---
+++ RDMS-Fundamental/covid_vaccine.sql
@@ -0,0 +1,7 @@
+CREATE TABLE covid_vaccines (
+    country VARCHAR(50),
+    report_date DATE,
+    vaccinated_people INT,
+    fully_vaccinated INT,
+    booster_doses INT
+);
